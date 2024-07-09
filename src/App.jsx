9ec65e4ea0b2ed9b15c6f935ec8227dddb69b9dc
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2024-07-09 10:52:07 +0100

    chore: react drag drop package added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import TextInImage from "./components/TextInImage"
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
      <div className="App">
       <p>This AI image generator, is built using <a href="https://autogon.ai/playground/text-in-image" target='_blank'>Autogon AI</a> text in image api (OCR)</p>
+      <TextInImage />
      </div>
     </>
   )
---
+++ src/components/TextInImage.jsx
@@ -0,0 +1,11 @@
+import ImageUpload from "./ImageUpload"
+
+function TextInImage() {
+  return (
+    <div>
+      <ImageUpload />
+    </div>
+  )
+}
+
+export default TextInImage
