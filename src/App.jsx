2804eecb4d67b9045a85ed36d49720224c21f54b
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2024-07-09 10:36:29 +0100

    chore: code setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <div className="App">
+      <p>This AI image generator, is built using <a href="https://autogon.ai/playground/text-in-image" target='_blank'>Autogon AI</a> text in image api (OCR)</p>
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Loading.jsx
@@ -0,0 +1,6 @@
+const Loading = () => {
+    return <div className="custom-loader"></div>;
+  };
+  
+  export default Loading;
+  
