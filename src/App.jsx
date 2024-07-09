3d239d9bacb7e56030a4688186dbd579fa401a8c
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2024-07-09 12:19:24 +0100

    feat: add image uplaod and conversion code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import TextInImage from "./components/TextInImage"
+import ImageUpload from "./components/ImageUpload"
 
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
      <div className="App">
       <h1>Text In Image Detection</h1>
       <p>This Text in image Detection, is built using <a href="https://autogon.ai/playground/text-in-image" target='_blank'>Autogon AI</a> text in image api (OCR)</p>
-      <TextInImage />
+      <ImageUpload />
      </div>
     </>
   )
--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -1,15 +1,64 @@
 import { useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
+import Loading from "./Loading"
 
 const fileTypes = ["JPEG", "PNG", "JPG"];
 
 export default function ImageUpload() {
   const [file, setFile] = useState(null);
+  const [base64Image, setBase64Image] = useState("");
+  const [apiRes, setApiRes] = useState(null);
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
+  const API_KEY = import.meta.env.VITE_API_KEY;
+
   const handleChange = (file) => {
     setFile(file);
+    convertToBase64(file);
   };
+
+  const convertToBase64 = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setBase64Image(reader.result);
+    };
+    reader.onerror = (error) => {
+      console.error("Error converting file to base64:", error);
+    };
+  };
+
+  const handleAnalyzeImage = async () => {
+    if (!base64Image) {
+      alert("No image available");
+      return;
+    }
+
+    try {
+      const res = await fetch(`${BASE_URL}/services/vision-ai/`, {
+        method: "POST",
+        headers: {
+          "X-AUG-Key": API_KEY,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          image: base64Image.split(',')[1], // Remove the data:image/*;base64, part
+          operation: "text_detection"
+        })
+      });
+      const data = await res.json();
+      setApiRes(data);
+    } catch (error) {
+      console.error("Error making API call:", error);
+    }
+  };
+
   return (
     <div className="App">
+        {loading && (
+        <div className="loading-overlay">
+          <Loading/>
+        </div>
+      )}
       <p>Drag your image into the box below or click the box to upload (Max size: 2mb)</p>
       <FileUploader
         multiple={false}
@@ -19,6 +68,8 @@ export default function ImageUpload() {
         required={true}
       />
       <p>{file ? `File name: ${file.name}` : "no files uploaded yet"}</p>
+      {base64Image && <button onClick={handleAnalyzeImage}>Analyze Image</button>}
+      {apiRes && <div><strong>API Response:</strong> {JSON.stringify(apiRes)}</div>}
     </div>
   );
 }
